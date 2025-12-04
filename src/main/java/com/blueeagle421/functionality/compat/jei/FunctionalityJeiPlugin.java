9db498c6ba551a5e72b3a036b7a3f262231488e2
BlueEagle421 <joachim.wielewski@gmail.com>
2025-12-04 09:48:03 +0100

    Add JEI plugin for repairable items

---
+++ src/main/java/com/blueeagle421/functionality/compat/jei/FunctionalityJeiPlugin.java
@@ -0,0 +1,77 @@
+package com.blueeagle421.functionality.compat.jei;
+
+import com.blueeagle421.functionality.FunctionalityMod;
+import com.blueeagle421.functionality.item.ModItems;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.RecipeTypes;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import mezz.jei.api.recipe.vanilla.IJeiAnvilRecipe;
+import mezz.jei.api.recipe.vanilla.IVanillaRecipeFactory;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Blocks;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+@JeiPlugin
+public class FunctionalityJeiPlugin implements IModPlugin {
+
+    private static final Map<Item, ItemStack> REPAIRABLE_ITEMS = Map.of(
+            ModItems.FINS.get(), new ItemStack(Items.SCUTE),
+            ModItems.OBSIDIAN_FINS.get(), new ItemStack(Items.OBSIDIAN));
+
+    @SuppressWarnings("removal")
+    @Override
+    public ResourceLocation getPluginUid() {
+        return new ResourceLocation(FunctionalityMod.MOD_ID, "jei_plugin");
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
+        IVanillaRecipeFactory factory = registration.getVanillaRecipeFactory();
+        List<IJeiAnvilRecipe> recipes = new ArrayList<>();
+
+        for (Map.Entry<Item, ItemStack> entry : REPAIRABLE_ITEMS.entrySet()) {
+            ItemStack itemStack = new ItemStack(entry.getKey());
+            ItemStack repairMaterial = entry.getValue();
+
+            if (!itemStack.isDamageableItem())
+                continue;
+
+            ItemStack dmg100 = itemStack.copy();
+            dmg100.setDamageValue(dmg100.getMaxDamage());
+
+            ItemStack dmg75 = itemStack.copy();
+            dmg75.setDamageValue(itemStack.getMaxDamage() * 3 / 4);
+
+            ItemStack dmg50 = itemStack.copy();
+            dmg50.setDamageValue(itemStack.getMaxDamage() / 2);
+
+            // repair material
+            recipes.add(factory.createAnvilRecipe(
+                    dmg100,
+                    Collections.singletonList(repairMaterial),
+                    Collections.singletonList(dmg75)));
+
+            // repair itself
+            recipes.add(factory.createAnvilRecipe(
+                    dmg75,
+                    Collections.singletonList(dmg75.copy()),
+                    Collections.singletonList(dmg50)));
+        }
+
+        registration.addRecipes(RecipeTypes.ANVIL, recipes);
+    }
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+        registration.addRecipeCatalyst(new ItemStack(Blocks.ANVIL), RecipeTypes.ANVIL);
+    }
+}
