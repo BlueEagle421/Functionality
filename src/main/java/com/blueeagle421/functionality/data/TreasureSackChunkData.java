5899bf895bb339d509e82fd48761ec59e91dc8fc
BlueEagle421 <joachim.wielewski@gmail.com>
2025-12-12 21:22:50 +0100

    Add treasure sack chunk limits

---
+++ src/main/java/com/blueeagle421/functionality/data/TreasureSackChunkData.java
@@ -0,0 +1,99 @@
+package com.blueeagle421.functionality.data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.world.level.saveddata.SavedData;
+
+public class TreasureSackChunkData extends SavedData {
+    private static final String DATA_NAME = "functionality_treasure_sack_chunk_data";
+    private static final String TAG_ENTRIES = "Entries";
+    private static final String TAG_CHUNK = "Chunk";
+    private static final String TAG_COUNT = "Count";
+    private static final String TAG_LAST_DAY = "LastDay";
+
+    public static final int DEFAULT_MAX = 30;
+    public static final int DAILY_REGEN = 5;
+
+    private final Map<Long, Integer> counts = new HashMap<>();
+    private long lastProcessedDay = -1L;
+
+    public TreasureSackChunkData() {
+    }
+
+    @Override
+    public CompoundTag save(CompoundTag nbt) {
+        ListTag entries = new ListTag();
+        for (Map.Entry<Long, Integer> e : counts.entrySet()) {
+            CompoundTag ent = new CompoundTag();
+            ent.putLong(TAG_CHUNK, e.getKey());
+            ent.putInt(TAG_COUNT, e.getValue());
+            entries.add(ent);
+        }
+        nbt.put(TAG_ENTRIES, entries);
+        nbt.putLong(TAG_LAST_DAY, lastProcessedDay);
+        return nbt;
+    }
+
+    public static TreasureSackChunkData load(CompoundTag nbt) {
+        TreasureSackChunkData data = new TreasureSackChunkData();
+        if (nbt.contains(TAG_ENTRIES)) {
+            ListTag entries = nbt.getList(TAG_ENTRIES, 10); // 10 = Compound
+            for (int i = 0; i < entries.size(); i++) {
+                CompoundTag ent = entries.getCompound(i);
+                long chunk = ent.getLong(TAG_CHUNK);
+                int count = ent.getInt(TAG_COUNT);
+                data.counts.put(chunk, count);
+            }
+        }
+        if (nbt.contains(TAG_LAST_DAY)) {
+            data.lastProcessedDay = nbt.getLong(TAG_LAST_DAY);
+        }
+        return data;
+    }
+
+    public int getCurrentForChunkOrInit(long chunkLong) {
+        return counts.computeIfAbsent(chunkLong, k -> DEFAULT_MAX);
+    }
+
+    public boolean consumeOne(long chunkLong) {
+        int cur = counts.getOrDefault(chunkLong, DEFAULT_MAX);
+        if (cur <= 0) {
+            counts.put(chunkLong, 0);
+            return false;
+        } else {
+            counts.put(chunkLong, cur - 1);
+            setDirty();
+            return true;
+        }
+    }
+
+    public void dailyRegen() {
+        boolean changed = false;
+        for (Map.Entry<Long, Integer> e : counts.entrySet()) {
+            int before = e.getValue();
+            int after = Math.min(DEFAULT_MAX, before + DAILY_REGEN);
+            if (after != before) {
+                e.setValue(after);
+                changed = true;
+            }
+        }
+        if (changed)
+            setDirty();
+    }
+
+    public long getLastProcessedDay() {
+        return lastProcessedDay;
+    }
+
+    public void setLastProcessedDay(long day) {
+        this.lastProcessedDay = day;
+        setDirty();
+    }
+
+    public static String storageKey() {
+        return DATA_NAME;
+    }
+}
---
+++ src/main/java/com/blueeagle421/functionality/event/treasureSack/OnLivingDrops.java
@@ -0,0 +1,51 @@
+package com.blueeagle421.functionality.event.treasureSack;
+
+import com.blueeagle421.functionality.FunctionalityMod;
+import com.blueeagle421.functionality.item.custom.TreasureSackItem;
+import com.blueeagle421.functionality.data.TreasureSackChunkData;
+
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.Entity.RemovalReason;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.core.BlockPos;
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = FunctionalityMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class OnLivingDrops {
+
+    @SubscribeEvent
+    public static void onLivingDrops(LivingDropsEvent event) {
+
+        if (!(event.getEntity().level() instanceof ServerLevel server))
+            return;
+
+        // read or create data
+        TreasureSackChunkData data = server.getDataStorage()
+                .computeIfAbsent(TreasureSackChunkData::load, TreasureSackChunkData::new,
+                        TreasureSackChunkData.storageKey());
+
+        for (ItemEntity itemEntity : event.getDrops()) {
+            if (itemEntity.getItem().getItem() instanceof TreasureSackItem) {
+
+                long chunkLong = getChunk(itemEntity.blockPosition());
+                data.getCurrentForChunkOrInit(chunkLong);
+
+                // do we have more sacks here?
+                boolean allowed = data.consumeOne(chunkLong);
+
+                if (!allowed)
+                    itemEntity.remove(RemovalReason.DISCARDED);
+            }
+        }
+    }
+
+    private static long getChunk(BlockPos pos) {
+        // 4 right bits shifts same as dividing by 16 and flooring
+        int chunkX = pos.getX() >> 4;
+        int chunkZ = pos.getZ() >> 4;
+        return ChunkPos.asLong(chunkX, chunkZ);
+    }
+}
