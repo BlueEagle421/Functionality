40f8ad7e59954a80d7043d0d00ece61eaf877b14
BlueEagle421 <joachim.wielewski@gmail.com>
2025-12-15 12:14:15 +0100

    Clean up mixin

--- src/main/java/com/blueeagle421/functionality/mixin/throwableDiscs/ItemEnchantabilityMixin.java
+++ src/main/java/com/blueeagle421/functionality/mixin/throwableDiscs/ItemEnchantabilityMixin.java
@@ -3,7 +3,6 @@ package com.blueeagle421.functionality.mixin.throwableDiscs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.RecordItem;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,18 +11,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import com.blueeagle421.functionality.config.FunctionalityConfig;
 import com.blueeagle421.functionality.config.subcategories.features.ThrowableDiscs;
 
-@Mixin(Item.class)
+@Mixin(value = Item.class)
 public abstract class ItemEnchantabilityMixin {
 
     @Inject(method = "isEnchantable", at = @At("HEAD"), cancellable = true)
-    private void makeStickEnchantable(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
+    private void makeRecordEnchantable(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
         if (config().enabled.get() && stack.getItem() instanceof RecordItem) {
             cir.setReturnValue(true);
         }
     }
 
     @Inject(method = "getEnchantmentValue", at = @At("HEAD"), cancellable = true)
-    private void giveStickEnchantValue(CallbackInfoReturnable<Integer> cir) {
+    private void giveRecordEnchantValue(CallbackInfoReturnable<Integer> cir) {
         Item self = (Item) (Object) this;
         if (config().enabled.get() && self instanceof RecordItem) {
             cir.setReturnValue(14); // same as iron tools
