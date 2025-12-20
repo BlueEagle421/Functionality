e2837d7e2e9fd247caa1c1a205c82c1f70a0a30c
BlueEagle421 <joachim.wielewski@gmail.com>
2025-12-20 22:48:24 +0100

    Remove glow flame particle

--- src/main/java/com/blueeagle421/functionality/particle/ModParticles.java
+++ src/main/java/com/blueeagle421/functionality/particle/ModParticles.java
@@ -13,9 +13,6 @@ public class ModParticles {
     public static final DeferredRegister<ParticleType<?>> PARTICLE_TYPES = DeferredRegister
             .create(ForgeRegistries.PARTICLE_TYPES, FunctionalityMod.MOD_ID);
 
-    public static final RegistryObject<SimpleParticleType> GLOW_FLAME = PARTICLE_TYPES
-            .register("glow_flame", () -> new SimpleParticleType(true));
-
     public static final RegistryObject<SimpleParticleType> GLOW_SMOKE = PARTICLE_TYPES
             .register("glow_smoke", () -> new SimpleParticleType(true));
 
