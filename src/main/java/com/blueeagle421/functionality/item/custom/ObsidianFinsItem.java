802cb8b18216d337b97754f4271796c9fd9338d6
BlueEagle421 <joachim.wielewski@gmail.com>
2025-12-23 18:00:13 +0100

    Add obsidian fins tex

--- src/main/java/com/blueeagle421/functionality/item/custom/ObsidianFinsItem.java
+++ src/main/java/com/blueeagle421/functionality/item/custom/ObsidianFinsItem.java
@@ -1,6 +1,8 @@
 package com.blueeagle421.functionality.item.custom;
 
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.ItemStack;
@@ -10,12 +12,23 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
+import com.blueeagle421.functionality.FunctionalityMod;
 import com.blueeagle421.functionality.utils.TooltipUtils;
 
 //lava swimming methods are in mixins (the class is important for type checks)
 public class ObsidianFinsItem extends ArmorItem {
     public ObsidianFinsItem(ArmorMaterial material, Type type, Properties properties) {
         super(material, type, properties);
+    }
+
+    @Override
+    public String getArmorTexture(
+            ItemStack stack,
+            Entity entity,
+            EquipmentSlot slot,
+            String type) {
+
+        return FunctionalityMod.MOD_ID + ":textures/models/armor/obsidian_fins.png";
     }
 
     @Override
