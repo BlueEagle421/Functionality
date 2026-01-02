97b00d5a6073edf73b90276c53626f6b77a0c74f
BlueEagle421 <joachim.wielewski@gmail.com>
2026-01-03 00:05:27 +0100

    Remove unused local variables

--- src/main/java/com/blueeagle421/functionality/client/InfernoGearHumanoidModel.java
+++ src/main/java/com/blueeagle421/functionality/client/InfernoGearHumanoidModel.java
@@ -36,12 +36,12 @@ public class InfernoGearHumanoidModel<T extends LivingEntity> extends HumanoidMo
 		PartDefinition inferno_gear = part.addOrReplaceChild("inferno_gear", CubeListBuilder.create(),
 				PartPose.offset(0.0F, 12.0F, 0.0F));
 
-		PartDefinition cube_r1 = inferno_gear.addOrReplaceChild("cube_r1",
+		inferno_gear.addOrReplaceChild("cube_r1",
 				CubeListBuilder.create().texOffs(0, 16).addBox(0.1F, 0.75F, 2.0F, 4.0F, 12.0F, 4.0F,
 						new CubeDeformation(0.0F)),
 				PartPose.offsetAndRotation(0.0F, -12.5F, 0.0F, 0.0F, 0.0873F, -0.0436F));
 
-		PartDefinition cube_r2 = inferno_gear.addOrReplaceChild("cube_r2",
+		inferno_gear.addOrReplaceChild("cube_r2",
 				CubeListBuilder.create().texOffs(0, 0).addBox(-3.9F, 0.75F, 2.0F, 4.0F, 12.0F, 4.0F,
 						new CubeDeformation(0.0F)),
 				PartPose.offsetAndRotation(0.0F, -12.5F, 0.0F, 0.0F, -0.0873F, 0.0436F));
