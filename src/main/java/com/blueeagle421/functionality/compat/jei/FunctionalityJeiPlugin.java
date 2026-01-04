4e0a5e63f47b202c0e3ac6bfaf5bb417ef185ce2
BlueEagle421 <joachim.wielewski@gmail.com>
2026-01-04 20:04:00 +0100

    Fix missing json recipes

--- src/main/java/com/blueeagle421/functionality/compat/jei/FunctionalityJeiPlugin.java
+++ src/main/java/com/blueeagle421/functionality/compat/jei/FunctionalityJeiPlugin.java
@@ -1,6 +1,7 @@
 package com.blueeagle421.functionality.compat.jei;
 
 import com.blueeagle421.functionality.FunctionalityMod;
+import com.blueeagle421.functionality.recipe.InformationRecipe;
 
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
@@ -9,11 +10,14 @@ import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
 import mezz.jei.api.recipe.vanilla.IVanillaRecipeFactory;
+import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeManager;
 import net.minecraft.world.level.block.Blocks;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @JeiPlugin
 public class FunctionalityJeiPlugin implements IModPlugin {
@@ -33,13 +37,19 @@ public class FunctionalityJeiPlugin implements IModPlugin {
     public void registerRecipes(IRecipeRegistration registration) {
         IVanillaRecipeFactory factory = registration.getVanillaRecipeFactory();
 
-        // Information recipes
+        RecipeManager recipeManager = Minecraft.getInstance().level.getRecipeManager();
+
+        // JSON information recipes
+        List<InformationRecipe> infoRecipes = recipeManager.getAllRecipesFor(InformationRecipe.Type.INSTANCE);
+        registration.addRecipes(InformationCategory.INFORMATION_TYPE, infoRecipes);
+
+        // Generated information recipes
         InformationRecipeGenerator.generateRecipes();
         registration.addRecipes(
                 InformationCategory.INFORMATION_TYPE,
                 new ArrayList<>(InformationRecipeGenerator.GENERATED.values()));
 
-        // Anvil recipes
+        // Generated anvil recipes
         registration.addRecipes(
                 RecipeTypes.ANVIL,
                 AnvilRecipeGenerator.generate(factory));
