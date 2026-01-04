4260e9bdfd11dd3ccb400747aa6945d6e8eb5f07
BlueEagle421 <joachim.wielewski@gmail.com>
2026-01-04 19:51:25 +0100

    Improve anvil recipe generation

---
+++ src/main/java/com/blueeagle421/functionality/compat/jei/AnvilRecipeGenerator.java
@@ -0,0 +1,102 @@
+package com.blueeagle421.functionality.compat.jei;
+
+import com.blueeagle421.functionality.config.FunctionalityConfig;
+import com.blueeagle421.functionality.item.ModItems;
+
+import mezz.jei.api.recipe.vanilla.IJeiAnvilRecipe;
+import mezz.jei.api.recipe.vanilla.IVanillaRecipeFactory;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.RecordItem;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@SuppressWarnings("deprecation")
+public class AnvilRecipeGenerator {
+
+    private static Map<Item, ItemStack> REPAIRABLE_ITEMS() {
+        return Map.of(
+                ModItems.HARPOON.get(), new ItemStack(Items.FLINT),
+                ModItems.BIDENT.get(), new ItemStack(Items.PRISMARINE_SHARD),
+                ModItems.GLOW_CROWN.get(), new ItemStack(Items.KELP),
+                ModItems.FINS.get(), new ItemStack(Items.SCUTE),
+                ModItems.OBSIDIAN_FINS.get(), new ItemStack(Items.OBSIDIAN),
+                ModItems.INFERNO_GEAR.get(), new ItemStack(Items.OBSIDIAN));
+    }
+
+    public static List<IJeiAnvilRecipe> generate(IVanillaRecipeFactory factory) {
+        List<IJeiAnvilRecipe> recipes = new ArrayList<>();
+
+        generateItemRepairRecipes(factory, recipes);
+        generateThrowableDiscRecipes(factory, recipes);
+
+        return recipes;
+    }
+
+    private static void generateItemRepairRecipes(
+            IVanillaRecipeFactory factory,
+            List<IJeiAnvilRecipe> recipes) {
+
+        for (Map.Entry<Item, ItemStack> entry : REPAIRABLE_ITEMS().entrySet()) {
+            addStandardRepairRecipes(
+                    factory,
+                    recipes,
+                    new ItemStack(entry.getKey()),
+                    entry.getValue());
+        }
+    }
+
+    private static void generateThrowableDiscRecipes(
+            IVanillaRecipeFactory factory,
+            List<IJeiAnvilRecipe> recipes) {
+
+        if (!FunctionalityConfig.COMMON.features.throwableDiscs.enabled.get())
+            return;
+
+        ItemStack fragment = FunctionalityConfig.COMMON.features.throwableDiscs
+                .getDiscRepairIngredient()
+                .getItems()[0];
+
+        BuiltInRegistries.ITEM.stream()
+                .filter(item -> item instanceof RecordItem)
+                .map(ItemStack::new)
+                .forEach(disc -> addStandardRepairRecipes(factory, recipes, disc, fragment));
+    }
+
+    private static void addStandardRepairRecipes(
+            IVanillaRecipeFactory factory,
+            List<IJeiAnvilRecipe> recipes,
+            ItemStack item,
+            ItemStack repairMaterial) {
+
+        if (!item.isDamageableItem())
+            return;
+
+        int maxDamage = item.getMaxDamage();
+
+        ItemStack dmg100 = withDamage(item, maxDamage);
+        ItemStack dmg75 = withDamage(item, maxDamage * 3 / 4);
+        ItemStack dmg50 = withDamage(item, maxDamage / 2);
+
+        recipes.add(factory.createAnvilRecipe(
+                dmg100,
+                List.of(repairMaterial),
+                List.of(dmg75)));
+
+        recipes.add(factory.createAnvilRecipe(
+                dmg75,
+                List.of(dmg75.copy()),
+                List.of(dmg50)));
+    }
+
+    private static ItemStack withDamage(ItemStack stack, int damage) {
+        ItemStack copy = stack.copy();
+        copy.setDamageValue(damage);
+        return copy;
+    }
+}
--- src/main/java/com/blueeagle421/functionality/compat/jei/FunctionalityJeiPlugin.java
+++ src/main/java/com/blueeagle421/functionality/compat/jei/FunctionalityJeiPlugin.java
@@ -1,9 +1,6 @@
 package com.blueeagle421.functionality.compat.jei;
 
 import com.blueeagle421.functionality.FunctionalityMod;
-import com.blueeagle421.functionality.config.FunctionalityConfig;
-import com.blueeagle421.functionality.item.ModItems;
-import com.blueeagle421.functionality.recipe.InformationRecipe;
 
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
@@ -11,35 +8,15 @@ import mezz.jei.api.constants.RecipeTypes;
 import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
-import mezz.jei.api.recipe.vanilla.IJeiAnvilRecipe;
 import mezz.jei.api.recipe.vanilla.IVanillaRecipeFactory;
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.RecordItem;
-import net.minecraft.world.item.crafting.RecipeManager;
 import net.minecraft.world.level.block.Blocks;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
 
 @JeiPlugin
 public class FunctionalityJeiPlugin implements IModPlugin {
-
-    private static Map<Item, ItemStack> REPAIRABLE_ITEMS() {
-        return Map.of(
-                ModItems.HARPOON.get(), new ItemStack(Items.FLINT),
-                ModItems.BIDENT.get(), new ItemStack(Items.PRISMARINE_SHARD),
-                ModItems.GLOW_CROWN.get(), new ItemStack(Items.KELP),
-                ModItems.FINS.get(), new ItemStack(Items.SCUTE),
-                ModItems.OBSIDIAN_FINS.get(), new ItemStack(Items.OBSIDIAN),
-                ModItems.INFERNO_GEAR.get(), new ItemStack(Items.OBSIDIAN));
-    }
 
     @SuppressWarnings("removal")
     @Override
@@ -52,84 +29,20 @@ public class FunctionalityJeiPlugin implements IModPlugin {
         registration.addRecipeCategories(new InformationCategory(registration.getJeiHelpers().getGuiHelper()));
     }
 
-    @SuppressWarnings("deprecation")
     @Override
     public void registerRecipes(IRecipeRegistration registration) {
         IVanillaRecipeFactory factory = registration.getVanillaRecipeFactory();
-        List<IJeiAnvilRecipe> recipes = new ArrayList<>();
 
-        RecipeManager recipeManager = Minecraft.getInstance().level.getRecipeManager();
-
-        List<InformationRecipe> infoRecipes = recipeManager.getAllRecipesFor(InformationRecipe.Type.INSTANCE);
-        registration.addRecipes(InformationCategory.INFORMATION_TYPE, infoRecipes);
-
+        // Information recipes
         InformationRecipeGenerator.generateRecipes();
-        List<InformationRecipe> list = new ArrayList<>(InformationRecipeGenerator.GENERATED.values());
-        registration.addRecipes(InformationCategory.INFORMATION_TYPE, list);
+        registration.addRecipes(
+                InformationCategory.INFORMATION_TYPE,
+                new ArrayList<>(InformationRecipeGenerator.GENERATED.values()));
 
-        for (Map.Entry<Item, ItemStack> entry : REPAIRABLE_ITEMS().entrySet()) {
-            ItemStack itemStack = new ItemStack(entry.getKey());
-            ItemStack repairMaterial = entry.getValue();
-
-            if (!itemStack.isDamageableItem())
-                continue;
-
-            ItemStack dmg100 = itemStack.copy();
-            dmg100.setDamageValue(dmg100.getMaxDamage());
-
-            ItemStack dmg75 = itemStack.copy();
-            dmg75.setDamageValue(itemStack.getMaxDamage() * 3 / 4);
-
-            ItemStack dmg50 = itemStack.copy();
-            dmg50.setDamageValue(itemStack.getMaxDamage() / 2);
-
-            // repair material
-            recipes.add(factory.createAnvilRecipe(
-                    dmg100,
-                    Collections.singletonList(repairMaterial),
-                    Collections.singletonList(dmg75)));
-
-            // repair itself
-            recipes.add(factory.createAnvilRecipe(
-                    dmg75,
-                    Collections.singletonList(dmg75.copy()),
-                    Collections.singletonList(dmg50)));
-        }
-
-        BuiltInRegistries.ITEM.stream()
-                .filter(i -> i instanceof RecordItem)
-                .forEach(i -> {
-                    ItemStack disc = new ItemStack(i);
-
-                    if (!disc.isDamageableItem())
-                        return; // skip non-damageable discs (safety)
-
-                    ItemStack dmg100 = disc.copy();
-                    dmg100.setDamageValue(dmg100.getMaxDamage());
-
-                    ItemStack dmg75 = disc.copy();
-                    // compute 75% as integer (floor)
-                    dmg75.setDamageValue(disc.getMaxDamage() * 3 / 4);
-
-                    ItemStack dmg50 = disc.copy();
-                    dmg50.setDamageValue(disc.getMaxDamage() / 2);
-
-                    // repair with fragment (use your mod's fragment item)
-                    recipes.add(factory.createAnvilRecipe(
-                            dmg100,
-                            Collections.singletonList(
-                                    FunctionalityConfig.COMMON.features.throwableDiscs.getDiscRepairIngredient()
-                                            .getItems()[0]), // fragment material shown
-                            Collections.singletonList(dmg75)));
-
-                    // combine two damaged discs to get a less-damaged disc
-                    recipes.add(factory.createAnvilRecipe(
-                            dmg75,
-                            Collections.singletonList(dmg75.copy()),
-                            Collections.singletonList(dmg50)));
-                });
-
-        registration.addRecipes(RecipeTypes.ANVIL, recipes);
+        // Anvil recipes
+        registration.addRecipes(
+                RecipeTypes.ANVIL,
+                AnvilRecipeGenerator.generate(factory));
     }
 
     @Override
