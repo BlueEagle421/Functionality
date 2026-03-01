e6fad67212e327ae6d889914ffd71499e23f9892
BlueEagle421 <joachim.wielewski@gmail.com>
2026-03-01 11:13:20 +0100

    Fix durability mixin for record items

--- src/main/java/com/blueeagle421/functionality/mixin/throwableDiscs/ItemsMixin.java
+++ src/main/java/com/blueeagle421/functionality/mixin/throwableDiscs/ItemsMixin.java
@@ -1,17 +1,25 @@
 package com.blueeagle421.functionality.mixin.throwableDiscs;
 
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.RecordItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 import com.blueeagle421.functionality.config.subcategories.features.ThrowableDiscs;
 
-@Mixin(net.minecraft.world.item.Items.class)
-public class ItemsMixin {
+@Mixin(RecordItem.class)
+public abstract class ItemsMixin {
 
-    @ModifyArg(method = "<clinit>", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/RecordItem;<init>(ILnet/minecraft/sounds/SoundEvent;Lnet/minecraft/world/item/Item$Properties;I)V"), index = 2)
-    private static Item.Properties addDurabilityToRecordItem(Item.Properties props) {
+    // Constructor 1
+    @ModifyArg(method = "<init>(ILjava/util/function/Supplier;Lnet/minecraft/world/item/Item$Properties;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/Item;<init>(Lnet/minecraft/world/item/Item$Properties;)V"), index = 0)
+    private static Item.Properties modifyPropsSupplier(Item.Properties props) {
+        return props.durability(ThrowableDiscs.DURABILITY);
+    }
+
+    // Constructor 2
+    @ModifyArg(method = "<init>(ILnet/minecraft/sounds/SoundEvent;Lnet/minecraft/world/item/Item$Properties;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/Item;<init>(Lnet/minecraft/world/item/Item$Properties;)V"), index = 0)
+    private static Item.Properties modifyPropsSound(Item.Properties props) {
         return props.durability(ThrowableDiscs.DURABILITY);
     }
 }
